b036a4a2531514f378d35d529d04ff4c2914b272
shanjinghao <sjh.js@qq.com>
2023-12-12 07:31:15 +0800

    服务类-修改原来的PostService

--- src/modules/content/content.module.ts
+++ src/modules/content/content.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { PostController } from './controllers/post.controller';
+import { PostRepository } from './repositories';
 import { PostService } from './services/post.service';
 
 @Module({
   controllers: [PostController],
-  providers: [PostService],
+  providers: [PostService, PostRepository],
   exports: [PostService],
 })
 export class ContentModule {}
