dfad9ba06125bb15c9f9b15a8eece96d33dc5e13
shanjinghao <sjh.js@qq.com>
2023-12-12 07:43:05 +0800

    注入与调试

--- src/modules/content/content.module.ts
+++ src/modules/content/content.module.ts
@@ -1,12 +1,23 @@
+// src/modules/content/content.module.ts
 import { Module } from '@nestjs/common';
 
-import { PostController } from './controllers/post.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { DatabaseModule } from '../database/database.module';
+
+import { PostController } from './controllers';
+import { PostEntity } from './entities/post.entity';
 import { PostRepository } from './repositories';
-import { PostService } from './services/post.service';
+import { PostService, SanitizeService } from './services';
+import { PostSubscriber } from './subscribers';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([PostEntity]),
+    DatabaseModule.forRepository([PostRepository]),
+  ],
   controllers: [PostController],
-  providers: [PostService, PostRepository],
-  exports: [PostService],
+  providers: [PostService, PostSubscriber, SanitizeService],
+  exports: [PostService, DatabaseModule.forRepository([PostRepository])],
 })
 export class ContentModule {}
