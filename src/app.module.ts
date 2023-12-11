fb4e5da4d2557f229a82044c5265c9228dc79955
shanjinghao <sjh.js@qq.com>
2023-12-12 06:21:54 +0800

    处理移除代码后的报错

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ContentModule } from './modules/content/content.module';
-import { CoreModule } from './modules/core/core.module';
 import { DatabaseModule } from './modules/database/database.module';
-import { ExampleModule } from './modules/example/example.module';
-import { CoreModule } from './modules/core/core.module';
 
 @Module({
-  imports: [
-    ContentModule,
-    ExampleModule,
-    CoreModule.forRoot({
-      config: {
-        name: '3R教室',
-      },
-    }),
-    CoreModule,
-    DatabaseModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ContentModule, DatabaseModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
